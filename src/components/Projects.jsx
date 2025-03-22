cc5714ed7e33bbcf55574021a58a72cc38cdd6cf
Jakub Svoboda <jacubsvoboda@gmail.com>
2025-03-22 15:27:36 +0100

    img-workingmaybe

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects() {
     <div className="projects">
       {projects.map((project) => (
         <div className="projects__wrapper" key={project.id}>
-          <div className="projects__frame-label">SITE FRAME</div>
+          <div className="projects__frame-label">{project.name}</div>
           <a
             href={project.url}
             target="_blank"
