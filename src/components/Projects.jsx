3cbf39c0762ac9c607fae17fa131eb9293663181
Jakub Svoboda <jacubsvoboda@gmail.com>
2025-03-22 20:15:35 +0100

    array-working

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,38 +2,33 @@ import React from 'react'
 import projects from '../assets/data/projects.json'
 
 export default function Projects() {
-  const weeks = [...new Set(projects.map(project => project.week))].sort();
-
   return (
     <>
-      {weeks.map((week) => {
-        const weekProjects = projects.filter((project) => project.week === week);
-        return weekProjects.length > 0 ? (
-          <React.Fragment key={week}>
-            <div className="intro">
-              <p className="intro-left">WEEKLY DROPS</p>
-              <p className="intro-week">W-{week + 1}</p>
-              <p className="intro-right">FREE TIME INITIATIVE</p>
-            </div>
+      {projects.map((weekProjects, weekIndex) => (
+        <React.Fragment key={weekIndex}>
+          <div className="intro">
+            <p className="intro-left">WEEKLY DROPS</p>
+            <p className="intro-week">W-{weekIndex}</p>
+            <p className="intro-right">FREE TIME INITIATIVE</p>
+          </div>
 
-            <div className="projects">
-              {weekProjects.map((project) => (
-                <div className="projects__wrapper" key={project.id}>
-                  <div className="projects__frame-label">{project.name}</div>
-                  <a
-                    href={project.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="projects__item"
-                  >
-                    <img src={project.image} alt={project.name} />
-                  </a>
-                </div>
-              ))}
-            </div>
-          </React.Fragment>
-        ) : null;
-      })}
+          <div className="projects">
+            {weekProjects.map((project, projectIndex) => (
+              <div className="projects__wrapper" key={projectIndex}>
+                <div className="projects__frame-label">{project.name}</div>
+                <a
+                  href={project.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="projects__item"
+                >
+                  <img src={project.image} alt={project.name} />
+                </a>
+              </div>
+            ))}
+          </div>
+        </React.Fragment>
+      ))}
     </>
   );
 }
