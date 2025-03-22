33fe4a7f8d8bd0983a8c4e95021cad38b8102c46
Jakub Svoboda <jacubsvoboda@gmail.com>
2025-03-22 18:21:31 +0100

    img-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,8 @@ import Description from './components/Description'
 function App() {
   return (
     <Canvas
-      style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh' }}
       camera={{ position: [0, 0, 5], fov: 50 }}
-      frameloop="demand"
     >
-      <color attach="background" args={['#181418']} />
       <Suspense fallback={null}>
         <ScrollControls pages={3} damping={0.1} distance={1}>
           <Scroll html>
