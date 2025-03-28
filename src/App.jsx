f11bf4cc3a9c3e5d843c9d056e3d6bbb155673bf
Jakub Svoboda <jacubsvoboda@gmail.com>
2025-03-28 20:40:37 +0100

    bg-color-push

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/01-header/Header'
 import Projects from './components/02-projects/Projects'
 import Footer from './components/03-footer/Footer'
 
-const App = React.forwardRef((props, ref) => (
+const App = React.forwardRef((_, ref) => (
   <div id="app" className='app' ref={ref}>
     <Header />
     <Projects />
