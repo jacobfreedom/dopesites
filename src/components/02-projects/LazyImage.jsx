10dad956eec7411780b6a578341972270eae8daf
Jakub Svoboda <jacubsvoboda@gmail.com>
2025-04-02 23:01:32 +0200

    lazyimg-simplified

--- src/components/02-projects/LazyImage.jsx
+++ src/components/02-projects/LazyImage.jsx
@@ -1,23 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 export default function LazyImage({ src, alt, className }) {
   if (!src) {
     return null;
   }
-  const [imgSrc, setImgSrc] = useState(src);
-
-  useEffect(() => {
-    setImgSrc(src);
-  }, [src]);
-
+  
   return (
     <div className={`lazy-image-container ${className || ''}`}>
       <img
-        src={imgSrc}
+        src={src}
         alt={alt}
+        className="lazy-image-main lazy-image-visible"
         loading="lazy"
-        className="lazy-image-main"
-        onError={handleError}
       />
     </div>
   );
